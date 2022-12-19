34dcae55f8ce9ba6a5bcb5c76e13a3dfe93efebd
Martin Ferenda <mferenda@student.foi.hr>
2022-12-19 01:07:06 +0100

    Dodane aktivnosti i navigacija ekrana uputa za korištenje aplikacije, te aktivnost registracije.

--- app/src/main/java/com/example/mtoilet/MainActivity.kt
+++ app/src/main/java/com/example/mtoilet/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.mtoilet
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.google.android.material.button.MaterialButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val goButton = findViewById<Button>(R.id.go)
+        goButton.setOnClickListener {
+            val intent = Intent(this, Tutorial1::class.java)
+            startActivity(intent)
+        }
     }
 }
