4a70656b25272b30d9e50dc3c421c8795b32c42b
Jakov Gatarić <jgataric@foi.hr>
2022-12-24 21:18:45 +0100

    Dodana Publisher klasa i inicijalna verzija Gettera.

---
+++ MQTTPublisher/Getter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using MQTTnet;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
+
+HttpClient client = new HttpClient();
+//var response = client.GetAsync("docker.hub/api/getapi");
+
+var myData = new
+{
+    userId = "testUserId",
+    deviceId = "testDeviceId"
+};
+//Tranform it to Json object
+string jsonData = JsonConvert.SerializeObject(myData);
+
+Publisher publisher = new Publisher();
+await publisher.publish(jsonData);
---
+++ MQTTPublisher/Publisher.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using MQTTnet;
+using MQTTnet.Client;
+using System.Threading;
+using System.Threading.Tasks;
+
+class Publisher
+{
+     static MqttFactory mqttFactory = new MqttFactory();
+     static IMqttClient client = mqttFactory.CreateMqttClient();
+     static MqttClientOptions options = new MqttClientOptionsBuilder()
+                   .WithClientId(Guid.NewGuid().ToString())
+                   .WithTcpServer("test.mosquitto.org", 1883)
+                   .WithCleanSession()
+                   .Build();
+
+    public async Task publish(string payload)
+    {
+
+        await client.ConnectAsync(options);
+        Console.WriteLine("Client connected: " + client.IsConnected );
+
+        var applicationMessage = new MqttApplicationMessageBuilder()
+                            .WithTopic("foi/air2221")
+                            .WithPayload(payload)
+                            .Build();
+       await client.PublishAsync(applicationMessage);
+    }
+}
