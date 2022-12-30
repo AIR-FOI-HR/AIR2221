9e2e589c1656b6fc6ceb5b846445a061f006e296
Zvonimir Koprek <zkoprek@student.foi.hr>
2022-12-31 00:08:00 +0100

    - Added QR Code page - Adjusted navbar design accordingly

--- mtoilet/components/Navbar.tsx
+++ mtoilet/components/Navbar.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 import React from "react";
 
 const navbar_a =
-  "flex w-4/5 py-2.5 px-7 text-center text-2xl items-center justify-center font-semibold transition duration-300 hover:bg-pg-grey";
+  "flex grow py-2.5 px-7 w-fit text-center text-2xl items-center justify-center font-semibold transition duration-300 hover:bg-pg-grey";
 
 function Navbar() {
   return (
@@ -18,6 +18,9 @@ function Navbar() {
       </Link>
       <Link href="/Statistics" className={navbar_a}>
         Statistics
+      </Link>
+      <Link href="/QRcode" className={navbar_a}>
+        QR Code
       </Link>
     </div>
   );
---
+++ mtoilet/pages/QRcode.tsx
@@ -0,0 +1,12 @@
+import Navbar from "../components/Navbar";
+import TableDevices from "../components/Table";
+
+function QRcode() {
+  return (
+    <>
+      <Navbar />
+    </>
+  );
+}
+
+export default QRcode;
